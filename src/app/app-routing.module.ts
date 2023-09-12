249aefd034688a98b15d4a4a57c3efe4082f90f4
emersonariel-ia <emersonariel123@gmail.com>
2023-09-12 10:25:11 -0300

    Estava gerando erro para o Erik II

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './core/components/login/login.component';
+import { CadastroComponent } from './core/components/cadastro/cadastro.component';
+import { HomeComponent } from './core/components/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'teste', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) }
 ];
 @NgModule({
   imports: [
@@ -13,4 +16,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
